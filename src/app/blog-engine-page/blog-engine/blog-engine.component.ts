95f836f333df22ce09f3ab7da3e1640c4c71e7b0
Leslie Ros <leslie.ros@ixblue.com>
2019-12-01 15:11:47 +0100

    blog engine page

---
+++ src/app/blog-engine-page/blog-engine/blog-engine.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
+
+@Component({
+  selector: 'app-blog-engine',
+  templateUrl: './blog-engine.component.html',
+  styleUrls: ['./blog-engine.component.css']
+})
+export class BlogEngineComponent {
+  constructor(public auth: AuthService) {
+    auth.handleAuthentication();
+  }
+}
